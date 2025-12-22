3b79d8dba27c90db8aa7e1b7cf555ccc931ad7c5
Manabu01 <poomipatboonkong@gmail.com>
2025-12-23 00:11:39 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+int printO(int n, int m){
+    if(n <= 0 || m <= 0){
+        cout << "Invalid input";
+    }else{
+        for(int j = 0; j < n; j++){
+            for(int i = 0; i < m; i++){
+                cout << "O";
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
 
 int main(){
 	
